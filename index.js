9e271a9b8306c50bd5f3e19797238ce2afc49f90
Shuvalov Anton <anton_shuvalov@me.com>
2014-02-17 19:22:04 +0600

    rm :message

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var Logger = module.exports = require('./lib/logger');
 
 var logger = new Logger(module, true);
-var x = logger.log('some logs');
-console.log(x);
-// logger.info('some infos');
-// logger.error('some errors');
-// logger.warn('some warns');
+logger.log('some logs');
+logger.info('some infos');
+logger.error('some errors');
+logger.warn('some warns');
