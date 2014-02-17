79f791e2442a95c30722577386d07003e07268f3
Shuvalov Anton <anton_shuvalov@me.com>
2014-02-17 18:45:12 +0600

    just work

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/logger');
+var Logger = module.exports = require('./lib/logger');
+
+var logger = new Logger(module, true);
+var x = logger.log('some logs');
+console.log(x);
+// logger.info('some infos');
+// logger.error('some errors');
+// logger.warn('some warns');
